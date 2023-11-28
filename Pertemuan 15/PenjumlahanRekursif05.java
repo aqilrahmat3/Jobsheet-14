6a31aa2f422a342745c49236044615bca24b5b81
aqilrahmat3 <144086802+aqilrahmat3@users.noreply.github.com>
2023-11-28 16:40:37 +0700

    :)

---
+++ Pertemuan 15/PenjumlahanRekursif05.java
@@ -0,0 +1,15 @@
+public class PenjumlahanRekursif05 {
+    
+    public static int jumlahRekursif(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n + jumlahRekursif(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 8;
+        int hasil = jumlahRekursif(n);
+        System.out.println("Hasil penjumlahan = " + hasil);
+    }
+}
